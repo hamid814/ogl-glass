0bed4f4b92e5295941999be22edbb79c31415aa5
hamid814 <hamid331994@gmail.com>
2022-12-16 05:27:11 -0800

    gh-pages

--- dist/public.e31bb0bc.js
+++ dist/public.e31bb0bc.js
@@ -14963,7 +14963,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58176" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60151" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
